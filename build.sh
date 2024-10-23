ee23fb21cf8bd52c6218fb20d22d3008d0ee7cde
Ilyar <ilyar.software@gmail.com>
2024-10-23 20:01:05 +0200

    build: setup release step

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 
-#git clone git@github.com:Layer2State/radar.git site
 npm run build:data && npm run build
-cd local/site
-find . -mindepth 1 -not -name '.git' -exec rm -rf {} +
-cp -r ../../out/* .
-cd local/site && git add . && git cm update && git pub
+npx gh-pages --repo git@github.com:Layer2State/radar.git --nojekyll --branch gh-pages --dist out
